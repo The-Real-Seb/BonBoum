f2c1adcae66b04aeced88d3bef001057adbf64c3
Sebastien chevallier <44536776+The-Real-Seb@users.noreply.github.com>
2024-02-14 14:48:38 +0100

    check main menu

--- Assets/Scripts/UI/Menus/MainMenuManager.cs
+++ Assets/Scripts/UI/Menus/MainMenuManager.cs
@@ -1,12 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenuManager : MonoBehaviour
 {
     public void Play()
     {
-        
+        SceneManager.LoadScene(1);
     }
 
     public void Quit()
